62f8317025420dd482028d7b186e17fd310fa348
russellntanh <russell.ntanh@gmail.com>
2025-03-16 22:27:06 +0700

    Add class how to generate salt-pepper noise

---
+++ ImageFilterApp/NoiseGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ImageFilterApp
+{
+    public static class NoiseGenerator
+    {
+        public static Bitmap SaltPepperNoiseGenerator(Bitmap input, double db)
+        {
+            Random random = new Random();
+            Bitmap output = new Bitmap(input);
+
+            double noiseRatio = Math.Pow(10, -db / 20);
+            int numberNoisyPixels = (int) (input.Width * input.Height * noiseRatio);
+
+            for (int i = 0; i < numberNoisyPixels; i ++)
+            {
+                int r = random.Next(input.Height);
+                int c = random.Next(input.Width);
+
+                Color noisePixel = random.NextDouble() < 0.5 ? Color.Black : Color.White;
+                output.SetPixel(c, r, noisePixel);
+            }
+
+            return output;
+        }
+    }
+}
